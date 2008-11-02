7c4cce26961b82d35c3bc87e50278f68e084d046
akm2000 <akm2000@8b017839-1745-0410-ac03-371ba388625b>
2008-11-02 17:42:21 +0000

    partial_scaffoldの0.1系
    git-svn-id: https://rbc-incubator.googlecode.com/svn/akm2000/plugins/trunk/partial_scaffold@514 8b017839-1745-0410-ac03-371ba388625b

--- init.rb
+++
@@ -1,4 +0,0 @@
-require "partial_scaffold"
-require "attrs_acceptable"
-::ActionController::AbstractRequest.send(:include, ::PartialScaffold::AjaxRedirectable::Request)
-::ActiveRecord::Base.send(:include, AttrsAcceptable)
